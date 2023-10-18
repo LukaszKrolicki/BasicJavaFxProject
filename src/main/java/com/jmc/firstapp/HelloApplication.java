979e5e32e67f6c547c5ff2dda08923eab8c95782
SesneiBunny <lukasmatrix12345@gmail.com>
2023-10-18 15:10:37 +0200

    Finishing first chapter

--- src/main/java/com/jmc/firstapp/HelloApplication.java
+++ src/main/java/com/jmc/firstapp/HelloApplication.java
@@ -1,23 +1,50 @@
 package com.jmc.firstapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
+import javafx.scene.layout.VBox;
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage){
+        Platform.setImplicitExit(false);
+
+        System.out.println(Thread.currentThread().getName());
+       VBox root = new VBox();
+       root.setAlignment(Pos.CENTER);
+       root.setPadding(new Insets(20,20,20,20));
+       root.setSpacing(20);
+       Label text= new Label("");
+
+       Button button= new Button("Click me");
+       button.setOnAction(e->text.setText("Exit"));
+
+       Button button2= new Button("Exit");
+       button2.setOnAction(e->{
+           Platform.exit();
+
+       });
+
+       root.getChildren().addAll(text,button,button2); //adding to the root
+
+        Scene scene = new Scene(root,350,300);
         stage.setScene(scene);
+        stage.setTitle("JavaFX application");
         stage.show();
     }
 
-    public static void main(String[] args) {
+    @Override
+    public void stop(){
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Closing application");
+    }
+
+    public static void main(String[] args){
         launch();
     }
 }
